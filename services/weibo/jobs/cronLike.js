68fdc0ee3102e085fafed6c2c8c3ed62966f7f6f
461437874@qq.com <461437874@qq.com>
2019-07-17 21:49:43 +0800

    add < 10

--- services/weibo/jobs/cronLike.js
+++ services/weibo/jobs/cronLike.js
@@ -1,3 +1,33 @@
+const request = require('request');
+
+function getLikeCount(swc, options){
+	return new Promise(resolve=>{
+		var option = {
+			url : 'https://weibo.cn/attitude/' + options.articleId,
+			headers : {
+				'cookie' : 'SUB=' + options.userInfo.sub
+			}
+		}
+
+		request(option, (err, res, body)=>{
+			if(err || res.statusCode != 200){
+				swc.log.error('get like count faile. article id -- ' + options.articleId);
+				resolve(undefined);
+				return ;
+			}
+
+			var MODEL = '<div class="pms" id="cmtfrm">';
+			if(body.indexOf(MODEL) < 0){
+				resolve(undefined);
+				return ;
+			}
+			body = body.substring(body.indexOf(MODEL) + MODEL.length);
+			var likeCount = body.substring(0, body.indexOf('人'));
+			resolve(parseInt(likeCount));
+		})
+	})
+}
+
 async function run(swc, options){
 	var ids = await swc.utils.weibo.getArticleByUid(swc, {
 		uid : options.uid
@@ -8,9 +38,21 @@ async function run(swc, options){
 		// 		articleId : ids[i]
 		// 	})
 		// }
-		await swc.services.weibo.handle.like(swc, {
+		var userInfos = await swc.services.weibo.dao.getUserInfos(swc, {});
+		var likeCount = await getLikeCount(swc, {
+			userInfo : userInfos[0],
 			articleId : ids[0]
 		})
+
+		if(likeCount && likeCount < 10){
+			await swc.services.weibo.handle.like(swc, {
+				articleId : ids[0]
+			})
+		} else if (!likeCount){
+			swc.log.error('get article like count error, articleId -- ' + ids[0]);
+		} else {
+
+		}
 	} else {
 		swc.log.error(`get article faile`);
 	}
