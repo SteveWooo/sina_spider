2ede77f5adcbc4e31de9914361c861e35b5faf60
461437874@qq.com <461437874@qq.com>
2019-07-17 20:00:30 +0800

    change uid

--- controllers/access.js
+++ controllers/access.js
@@ -6,7 +6,8 @@ module.exports = async (swc, options)=>{
 	await swc.services.weibo.init(swc, {});
 
 	await swc.services.weibo.jobs.cronLike(swc, {
-		uid : '1564541782', //stevewoo
+		// uid : '1564541782', //wenzhi
+		uid : '1976223032', //steve
 		// uid : '1923212617', //kimo
 	})
 	return swc;
